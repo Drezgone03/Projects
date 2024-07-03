c7c2993c11a6182c95b3b65a265f36b8964ab82e
Drezgone03 <drezga1993sid@gmail.com>
2024-07-03 11:25:30 +0200

    typeScript small Project

---
+++ Java_Script_projects/Type_Script_Project/src/app.ts
@@ -0,0 +1,32 @@
+import { Invoice } from "./modules/invoice.js";
+import { Payment } from "./modules/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./modules/ListTemplate.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+// inputs
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
+  if (type.value === "invoice") {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+  list.render(doc, type.value, "end");
+});
